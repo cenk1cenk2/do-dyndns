417e2bb6f745ea1beb0810c1491173ca0d7e8128
Cenk Kılıç <cenk@kilic.dev>
2022-04-18 22:11:14 +0200

    update branches

--- release.config.js
+++ release.config.js
@@ -1,61 +1,62 @@
 module.exports = {
-  repositoryUrl: 'git@github.com:cenk1cenk2/do-dyndns.git',
+  branches: ["master"],
+  repositoryUrl: "git@github.com:cenk1cenk2/do-dyndns.git",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      '@google/semantic-release-replace-plugin',
+      "@google/semantic-release-replace-plugin",
       {
         replacements: [
           {
-            files: ['version/v.go'],
+            files: ["version/v.go"],
             from: 'var Version string = ".*"',
             to: 'var Version string = "${nextRelease.version}"',
             results: [
               {
-                file: 'version/v.go',
+                file: "version/v.go",
                 hasChanged: true,
                 numMatches: 1,
-                numReplacements: 1
-              }
+                numReplacements: 1,
+              },
             ],
-            countMatches: true
-          }
-        ]
-      }
+            countMatches: true,
+          },
+        ],
+      },
     ],
-    '@semantic-release/changelog',
+    "@semantic-release/changelog",
     [
-      '@semantic-release/exec',
+      "@semantic-release/exec",
       {
-        generateNotesCmd: "echo 'latest,${nextRelease.version}' > .tags"
-      }
+        generateNotesCmd: "echo 'latest,${nextRelease.version}' > .tags",
+      },
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: ['version/v.go', 'CHANGELOG.md', 'README.md']
-      }
+        assets: ["version/v.go", "CHANGELOG.md", "README.md"],
+      },
     ],
     [
-      '@semantic-release/github',
+      "@semantic-release/github",
       {
         assets: [
           {
-            path: 'dist/do-dyndns-linux-amd64',
-            label: 'do-dyndns-linux-amd64_v<%= nextRelease.version %>'
+            path: "dist/do-dyndns-linux-amd64",
+            label: "do-dyndns-linux-amd64_v<%= nextRelease.version %>",
           },
           {
-            path: 'dist/do-dyndns-linux-armv7',
-            label: 'do-dyndns-linux-armv7_v<%= nextRelease.version %>'
+            path: "dist/do-dyndns-linux-armv7",
+            label: "do-dyndns-linux-armv7_v<%= nextRelease.version %>",
           },
           {
-            path: 'dist/do-dyndns-linux-arm64',
-            label: 'do-dyndns-linux-arm64_v<%= nextRelease.version %>'
-          }
-        ]
-      }
+            path: "dist/do-dyndns-linux-arm64",
+            label: "do-dyndns-linux-arm64_v<%= nextRelease.version %>",
+          },
+        ],
+      },
     ],
-    '@semantic-release/gitlab'
-  ]
-}
+    "@semantic-release/gitlab",
+  ],
+};
